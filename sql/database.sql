12117df49043e73108627f62c784ad2be0573322
Aberish <albert.nguyen.pro14@gmail.com>
2016-02-02 20:15:22 +0100

    add sql

---
+++ sql/database.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE vitrine;
+use vitrine;
+CREATE TABLE Products (
+    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(40) NOT NULL,
+    price INTEGER NOT NULL,
+	short_desc VARCHAR(60),
+	long_desc TEXT,
+	updated_at DATETIME,
+	created_at DATETIME,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Categories (
+    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(40) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Users (
+    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(40) NOT NULL,
+    mail VARCHAR(40) NOT NULL,
+	status VARCHAR(60),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Address (
+    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    country VARCHAR(20) NOT NULL,
+    address VARCHAR(60) NOT NULL,
+	zip_code INTEGER NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Wichlists (
+    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    product_id SMALLINT UNSIGNED NOT NULL ,
+	user_id SMALLINT UNSIGNED NOT NULL ,
+    `order_list` VARCHAR(60) NOT NULL,
+    PRIMARY KEY (id),
+	FOREIGN KEY (user_id) REFERENCES Users(id),
+	FOREIGN KEY (product_id) REFERENCES Products(id)
+);
+
+CREATE TABLE Products_categories (
+    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    product_id SMALLINT UNSIGNED NOT NULL,
+	category_id SMALLINT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+	FOREIGN KEY (category_id) REFERENCES Categories(id),
+	FOREIGN KEY (product_id) REFERENCES Products(id)
+);
