d2c138c66e26675e69d0178a3804adc94561c54f
yoannKy <y.keoxay@gmail.com>
2016-02-22 21:58:58 +0100

    updated layout

--- module/Backoffice/Module.php
+++ module/Backoffice/Module.php
@@ -19,8 +19,6 @@ class Module
         $serviceManager = $e->getApplication()->getServiceManager();
         $viewModel = $e->getApplication()->getMvcEvent()->getViewModel();
         $authService = $serviceManager->get('authentification_service');
-        $myService = $serviceManager->get('category_service');
-        $viewModel->categories = $myService->findAll();
         $viewModel->authService = $authService;
     }
 
